fb0e79ba5bcb218bb789e57e5885e595b44e63ec
chinen252-6 <satoukibi6@gmail.com>
2024-08-16 13:26:23 +0900

    ここまでlaravel教科書

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\ContactController;
+
+Route::get('post/mypost', [PostController::class, 'mypost'])->name('post.mypost');
+Route::get('post/mycomment', [PostController::class, 'mycomment'])->name('post.mycomment');
+Route::resource('post', PostController::class);
+Route::post('post/comment/store', [CommentController::class, 'store'])->name('comment.store');
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('top');
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [PostController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+
+
+
+require __DIR__.'/auth.php';
+
+
+
+Route::controller(ContactController::class)->group(function(){
+    Route::get('contact/create', 'create')->name('contact.create');
+    Route::post('contact/store', 'store')->name('contact.store');
+});
