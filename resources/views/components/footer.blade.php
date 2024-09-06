2e03c233c7ab9519f25aaf0bdc440b007c089bea
chinen252-6 <satoukibi6@gmail.com>
2024-09-06 22:43:53 +0900

    footer完了

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,21 @@
+<footer class="bg-gray-800 text-white py-13">
+    <div class="container mx-auto flex  items-center">
+        <!-- ロゴ部分 -->
+        <div class="flex items-center pt-6 " >
+            <img src="{{asset('logo/logo2.svg')}}" style="max-height:150px;" alt="せんべろロゴ" > 
+        </div>
+
+        <!-- プライバシーポリシーと利用規約 -->
+        <div class="ml-auto mr-0 pr-5">
+            <div class="mt-4  flex flex-col ml-auto mr-0">
+                <a href="{{ route('home') }}" class="text-gray-400 hover:text-white mx-2">HOME</a>
+                <a href="{{ route('store.create') }}"  class="text-gray-400 hover:text-white mx-2">お店登録</a>
+                <a href="{{ route('contact.create') }}" class="text-gray-400 hover:text-white mx-2">お問い合わせ</a>
+            </div>
+        </div>
+
+        
+    </div>
+    <p class="text-xs text-center pb-5">&copy; {{ date('Y') }} せんべろ探検隊</p>
+
+</footer>
